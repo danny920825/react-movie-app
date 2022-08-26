d88ece6c352fb721a863b5ecb02fae3b2d003922
Danny Paula <danny920825@gmail.com>
2022-08-26 01:26:07 -0400

    Api to SSL

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
 	const [searchValue, setSearchValue] = useState('');
 
 	const getMovieRequest = async (searchValue) => {
-		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=2d331a0b`;
+		const url = `https://www.omdbapi.com/?s=${searchValue}&apikey=2d331a0b`;
 
 		const response = await fetch(url);
 		const responseJson = await response.json();
